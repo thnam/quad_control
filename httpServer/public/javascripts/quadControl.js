393c594d072d5c2c2ded11a2ae033ca766aaf43f
Nam Tran <thnam.me@gmail.com>
2018-09-17 02:43:39 -0500

    forgot scripts

---
+++ httpServer/public/javascripts/quadControl.js
@@ -0,0 +1,9 @@
+$(() =>{
+$("#btn1Hz").click(()=>{ alert("1 Hz");})
+  $("#btn2Hz").click(()=>{ alert("2 Hz");})
+  $("#btn5Hz").click(()=>{ alert("5 Hz");})
+  $("#btn12Hz").click(()=>{ alert("12 Hz");})
+  $("#btnBurst").click(()=>{ alert("Burst");})
+  $("#btnExternal").click(()=>{ alert("External");})
+  $("#btnStop").click(()=>{ alert("Stop");})
+})
