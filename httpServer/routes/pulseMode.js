ad7d5337745dda32770583630215fda8a6d339b9
Nam Tran <thnam.me@gmail.com>
2018-09-21 20:51:54 -0500

    akdf

--- httpServer/routes/pulseMode.js
+++ httpServer/routes/pulseMode.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const httpLog = require(global.appRoot + '/loggers/httpLogger.js');
 const modeLog = require(global.appRoot + '/loggers/pulseModeLogger.js');
+const spawn = require('child_process').spawn;
 
 // use the same route for both set and get pulse mode
 router
@@ -14,13 +15,29 @@ router
     }
   })
   .post("/", (req, res) => {
-    httpLog.info("Pulse mode change request: from", req.body.currentMode, "to",
-      req.body.newMode); 
+    const currentMode = req.body.currentMode;
+    const newMode = req.body.newMode;
+    httpLog.info("Pulse mode change request: from " + currentMode + " to " + newMode); 
     // do some hardware stuff here first, should be try/catch and 
-    // .....
-    // then change mode info accordingly
-    modeLog.info(req.body.newMode);
-    res.sendStatus(200);
+    try {
+      // if (newMode == "Stop") {
+        var child = spawn(global.appRoot + "/../hwInterface/success");
+        // var child = spawn(global.appRoot + "/../hwInterface/failed");
+        child.on('exit', (code, signal) => {
+          if (code) {
+            httpLog.error("Error while changing pulse mode to " + newMode);
+            res.sendStatus(500);
+          }
+          else {
+            httpLog.info("Changed pulse mode to " + newMode);
+            modeLog.info(newMode);
+            res.sendStatus(200);
+          }
+        });
+      // }
+    } catch (e) {
+      next(e);
+    }
   })
 ;
 
