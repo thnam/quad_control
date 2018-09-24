949d6ec4d54a5aa487bcf63634d3a700ea5eb699
Nam Tran <thnam.me@gmail.com>
2018-09-24 11:53:23 -0500

    progress bars for voltage display

--- httpServer/public/javascripts/quadControl.js
+++ httpServer/public/javascripts/quadControl.js
@@ -70,5 +70,11 @@ socket.on("cv", (data) => {
   document.getElementById("pbNVSS").value = Math.abs(parseFloat(lastCV.fs.nv));
   document.getElementById("pbPVFS").value = parseFloat(lastCV.ss.pv);
   document.getElementById("pbNVFS").value = Math.abs(parseFloat(lastCV.ss.nv));
+  $("#valPVOS").html(lastCV.os.pv)
+  $("#valNVOS").html(lastCV.os.nv)
+  $("#valPVSS").html(lastCV.ss.pv)
+  $("#valNVSS").html(lastCV.ss.nv)
+  $("#valPVFS").html(lastCV.fs.pv)
+  $("#valNVFS").html(lastCV.fs.nv)
   // then cv table
 })
