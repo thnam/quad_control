2d48e7e5bcae139a392e094bad9f92cdc71f459f
Nam Tran <thnam.me@gmail.com>
2018-09-25 14:05:52 -0500

    display latest cv as a bar chart

--- httpServer/public/javascripts/cv.js
+++ httpServer/public/javascripts/cv.js
@@ -5,17 +5,65 @@ socket.on("cv", (data) => {
   // update the voltages and currents first
   const lastCV = JSON.parse(values[0].message);
 
-  document.getElementById("pbPVOS").value = parseFloat(lastCV.os.pv);
-  document.getElementById("pbNVOS").value = Math.abs(parseFloat(lastCV.os.nv));
-  document.getElementById("pbPVSS").value = parseFloat(lastCV.fs.pv);
-  document.getElementById("pbNVSS").value = Math.abs(parseFloat(lastCV.fs.nv));
-  document.getElementById("pbPVFS").value = parseFloat(lastCV.ss.pv);
-  document.getElementById("pbNVFS").value = Math.abs(parseFloat(lastCV.ss.nv));
-  $("#valPVOS").html(lastCV.os.pv)
-  $("#valNVOS").html(lastCV.os.nv)
-  $("#valPVSS").html(lastCV.ss.pv)
-  $("#valNVSS").html(lastCV.ss.nv)
-  $("#valPVFS").html(lastCV.fs.pv)
-  $("#valNVFS").html(lastCV.fs.nv)
+  window.lastCVChartData.datasets[0].data = [
+    parseFloat(lastCV.os.pv),
+    Math.abs(parseFloat(lastCV.os.nv)), 
+    parseFloat(lastCV.ss.pv),
+    Math.abs(parseFloat(lastCV.ss.nv)), 
+    parseFloat(lastCV.fs.pv),
+    Math.abs(parseFloat(lastCV.fs.nv))];
+
+  window.chartLastCV.update();
   // then cv table
 })
+
+window.onload = () => {
+  var ctxLastCV = document.getElementById("ctxLastCV");
+  window.lastCVChartData =  {
+    labels: ["PVOS", "NVOS", "PVSS", "NVSS", "PVFS", "NVFS"],
+    datasets: [{
+      label: 'kV, absolute value in case of negative voltage',
+      data: [20, 20, 20, 19, 14, 13.5],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  window.chartLastCV = new Chart(ctxLastCV, {
+    type: 'bar',
+    data: lastCVChartData,
+    options: {
+      responsive: true,
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: true,
+        text: 'Last voltages and currents readout'
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true,
+            suggestedMax: 30
+          }
+        }]
+      }
+    }
+  });
+}
