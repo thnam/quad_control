805814473c9b0c878fbedb86e2fdd70f58d08391
Nam Tran <thnam.me@gmail.com>
2018-09-25 14:17:53 -0500

    copy chart.js and datalabel plugin to scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,8 @@ module.exports = {
     name: 'http',
     script: 'httpServer/bin/www',
     watch: ["./httpServer/bin", "./httpServer/app.js",
-      "./httpServer/routes", "./httpServer/utils" ],
+      "./httpServer/routes", "./httpServer/utils", "./httpServer/views"
+    ],
     env: { NODE_ENV: "production" }
     // env: { NODE_ENV: "development" }
   }
