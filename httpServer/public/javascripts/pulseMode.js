486a06bc05c20a97d41eb2b383ecd9b9b5075e5a
Nam Tran <thnam.me@gmail.com>
2018-09-25 12:49:13 -0500

    pulse mode displayed

--- httpServer/public/javascripts/pulseMode.js
+++ httpServer/public/javascripts/pulseMode.js
@@ -16,7 +16,21 @@ function setPulseMode(newMode) {
           type: 'POST',
           url: baseUrl + '/pulsemode',
           data: {currentMode: currentMode, newMode: newMode},
-          success: (res) =>{console.log(res + ", pulse mode changed.")},
+          success: (res) =>{
+            console.log(res + ", pulse mode changed to " + newMode);
+
+            // change state indicator
+            $("#labelPulseMode").text(newMode);
+            if (newMode == "External") 
+              $("#labelPulseMode").css({ 'color': $(".btn-success").css("background-color"),
+                'font-size': '120%' });
+            else if (newMode == "Stop") 
+              $("#labelPulseMode").css({ 'color': $(".btn-danger").css("background-color"),
+                'font-size': '120%' });
+            else if (["1 Hz", "5 Hz", "10 Hz", "Burst", "Single"].includes(newMode))
+              $("#labelPulseMode").css({ 'color': $(".btn-info").css("background-color"),
+                'font-size': '120%' });
+          },
           error: (err, stat) =>{
             alert("Couldn't change pulse mode, error message: " + err.responseText);
           }
