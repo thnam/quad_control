362c0e844f9d7e0bc251848ad593bcd2a9ad92d0
Nam Tran <thnam.me@gmail.com>
2018-10-19 09:26:08 -0500

    change morgan message details

--- httpServer/app.js
+++ httpServer/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(morgan('tiny', {stream: httpLog.stream}));
+app.use(morgan('short', {stream: httpLog.stream}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
