65a16768b30d9da70446e11b8969d37a38892780
Nam Tran <thnam.me@gmail.com>
2019-02-20 11:09:51 -0600

    write to a local mysql server

--- dataLogger/loggers/cvLogger.js
+++ dataLogger/loggers/cvLogger.js
@@ -18,4 +18,37 @@ var cvOptions = {
   }
 };
 
-module.exports = require(global.appRoot + "/loggers/baseLogger.js")(cvOptions);
+const cvLog = require(global.appRoot + "/loggers/baseLogger.js")(cvOptions);
+
+// a hack for sql stuff
+const config = require('config');
+const knex = require("knex");
+// const knexfile = require('knexfile');
+
+var klient = knex({ client: "mysql", 
+  connection: config.get("sqlConnection")});
+
+cvLog.on("data", (data) =>{
+  console.log("sql: ", data.meta);
+  const vc = data.meta;
+  klient.table("voltages").insert({
+    time: new Date(),
+    ospv: vc.os.pv, 
+    osnv: vc.os.nv, 
+    ospc: vc.os.pc, 
+    osnc: vc.os.nc, 
+    sspv: vc.ss.pv, 
+    ssnv: vc.ss.nv, 
+    sspc: vc.ss.pc, 
+    ssnc: vc.ss.nc, 
+    fspv: vc.fs.pv, 
+    fsnv: vc.fs.nv, 
+    fspc: vc.fs.pc, 
+    fsnc: vc.fs.nc,
+    spark: vc.spark
+  })
+    .then(()=>{
+    console.log("inserted")});
+});
+
+module.exports = cvLog;
