4cb41b7899909bbeea9f67a1499111ca64038ce0
daq@gm2-01 <daq@gm2-01>
2019-09-12 13:23:06 -0500

    correctly send error message on failed command

--- httpServer/routes/pulseMode.js
+++ httpServer/routes/pulseMode.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const util = require('util');
 const router = express.Router();
 const httpLog = require(global.appRoot + '/loggers/httpLogger.js');
 const modeLog = require(global.appRoot + '/loggers/pulseModeLogger.js');
-const { exec } = require('child_process');
+const exec = util.promisify(require('child_process').exec);
+
 const BUEnv = {
   G2QUAD_ADDRESS_TABLE: "/home/daq/ESQ/host-carrier.trunk/os/software/g2quad/tables/Carrier.adt",
   ADDRESS_TABLE_PATH: "/home/daq/ESQ/host-carrier.trunk/os/software/g2quad/tables/"
@@ -22,58 +24,29 @@ router
     const newMode = req.body.mode;
     const env = req.app.get("env");
     httpLog.info("Pulse mode set request: " + newMode); 
-    try {
-      let cmd = "";
-      if (env === "production") {
-        // BU electronics interface
-        cmd += global.appRoot + "/../hwInterface/bu/setPulseMode";
-        cmd += ' -m "' + newMode + '"';
-        
-        // Sten module interface
-        // cmd += '/home/daq/gm2daq/frontends/ESQ_slow/quadcontrol/ljUtils/pulseControl.py --mode ';
-        // cmd += ' "' + newMode + '" --ntry 3';
-      } else 
-        cmd += global.appRoot + "/../hwInterface/success 2";
+    // try {
+    let cmd = "";
+    if (env === "production") {
+      // BU electronics interface
+      cmd += global.appRoot + "/../hwInterface/bu/setPulseMode";
+      cmd += ' -m "' + newMode + '"';
 
-      httpLog.info("Command:" + cmd);
-      var child = exec(cmd, {env: BUEnv});
+      // Sten module interface
+      // cmd += '/home/daq/gm2daq/frontends/ESQ_slow/quadcontrol/ljUtils/pulseControl.py --mode ';
+      // cmd += ' "' + newMode + '" --ntry 3';
+    } else 
+      cmd += global.appRoot + "/../hwInterface/success 2";
 
-      promiseFromChild(child).then(
-        function (result) {
-          httpLog.info("Changed pulse mode to " + newMode);
-          modeLog.info(newMode);
-          res.sendStatus(200);
-        }, 
-        function (err) {
-          httpLog.error(err);
-          httpLog.error("Error while changing pulse mode to " + newMode);
-          res.sendStatus(500);
-          throw(err);
-        }
-      );
-    } catch (e) {
-      next(e);
-    }
-
-    child.stdout.on("data", function (data) {
-      httpLog.info("setPulseMode stdout: " + data);
+    httpLog.info("Command:" + cmd);
+    exec(cmd, {env: BUEnv}).then(()=>{
+      httpLog.info("Changed pulse mode to " + newMode);
+      modeLog.info(newMode);
+      res.sendStatus(200);
+    }).catch((err)=>{
+      httpLog.error("Error while changing pulse mode to " + newMode);
+      httpLog.error(err.stderr);
+      res.status(500).send(err.stderr);
     });
-
-    child.stderr.on("data", function (data){
-      httpLog.error("setPulseMode stderr: " + data);
-    });
-
-    child.on("close", function(code){
-      httpLog.info("setPulseMode exit code: " + code);
-    })
-  })
-;
-
-function promiseFromChild(child) {
-  return new Promise(function (resolve, reject) {
-    child.addListener("error", reject);
-    child.addListener("exit", resolve);
   });
-};
 
 module.exports = router;
