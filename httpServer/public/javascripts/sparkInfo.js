0f5e284a8abddb09202740b674fd9924a0f6e1ab
daq@gm2-01 <daq@gm2-01>
2019-09-27 22:20:17 -0500

    use bootstrap table

--- httpServer/public/javascripts/sparkInfo.js
+++ httpServer/public/javascripts/sparkInfo.js
@@ -145,12 +145,11 @@ function clearSparkDisplay() {
 function showSparkHistory() {
   $.get(baseUrl + "/sparkHistory")
     .done((data)=>{
-      var dt = dynamicTable.config('sparkHistoryTable', 
-        ['f1', 'f2', 'f3'], // fields 1-3
-        ["Timestamp", "Spark pattern", "Spark bit"], //set to null for field names instead of custom header names
-        'There are no items to list...');
       var sparkData = [];
-      for (var i = 0, len = data.length; i < len; i++) {
+      console.log(data.length);
+      // for (var i = 0, len = data.length; i < len; i++) {
+      for (var i = 0, len = 10; i < len; i++) {
+        console.log(i, len);
         timestamp = new Date(data[i].timestamp);
         timestamp = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
 
@@ -174,9 +173,12 @@ function showSparkHistory() {
 
         sparkBit = data[i].meta.sparkBit;
 
-        sparkData.push({f1: timestamp, f2: patternStr, f3: sparkBit});
+        sparkData.push({"timestamp": timestamp,
+          "sparkPattern": patternStr, "sparkBit": sparkBit});
       }
-      dt.load(sparkData);
+      console.log(sparkData);
+      var $histSparkTable = $('#sparkHistoryTable');
+      $histSparkTable.bootstrapTable({data: sparkData});
 
     })
     .fail(()=>{
@@ -184,87 +186,3 @@ function showSparkHistory() {
     })
   
 }
-
-var dynamicTable = (function() {
-  var _tableId, _table, 
-    _fields, _headers, 
-    _defaultText;
-
-  /** Builds the row with columns from the specified names. 
-   *  If the item parameter is specified, the memebers of the names array will be used as property names of the item; otherwise they will be directly parsed as text.
-   */
-  function _buildRowColumns(names, item) {
-    var row = '<tr>';
-    if (names && names.length > 0)
-    {
-      $.each(names, function(index, name) {
-        var c = item ? item[name+''] : name;
-        row += '<td>' + c + '</td>';
-      });
-    }
-    row += '</tr>';
-    return row;
-  }
-
-  /** Builds and sets the headers of the table. */
-  function _setHeaders() {
-    // if no headers specified, we will use the fields as headers.
-    _headers = (_headers == null || _headers.length < 1) ? _fields : _headers; 
-    var h = _buildRowColumns(_headers);
-    if (_table.children('thead').length < 1) _table.prepend('<thead></thead>');
-    _table.children('thead').html(h);
-  }
-
-  function _setNoItemsInfo() {
-    if (_table.length < 1) return; //not configured.
-    var colspan = _headers != null && _headers.length > 0 ? 
-      'colspan="' + _headers.length + '"' : '';
-    var content = '<tr class="no-items"><td ' + colspan + ' style="text-align:center">' + 
-      _defaultText + '</td></tr>';
-    if (_table.children('tbody').length > 0)
-      _table.children('tbody').html(content);
-    else _table.append('<tbody>' + content + '</tbody>');
-  }
-
-  function _removeNoItemsInfo() {
-    var c = _table.children('tbody').children('tr');
-    if (c.length == 1 && c.hasClass('no-items')) _table.children('tbody').empty();
-  }
-
-  return {
-    /** Configres the dynamic table. */
-    config: function(tableId, fields, headers, defaultText) {
-      _tableId = tableId;
-      _table = $('#' + tableId);
-      _fields = fields || null;
-      _headers = headers || null;
-      _defaultText = defaultText || 'No items to list...';
-      _setHeaders();
-      _setNoItemsInfo();
-      return this;
-    },
-    /** Loads the specified data to the table body. */
-    load: function(data, append) {
-      if (_table.length < 1) return; //not configured.
-      _setHeaders();
-      _removeNoItemsInfo();
-      if (data && data.length > 0) {
-        var rows = '';
-        $.each(data, function(index, item) {
-          rows += _buildRowColumns(_fields, item);
-        });
-        var mthd = append ? 'append' : 'html';
-        _table.children('tbody')[mthd](rows);
-      }
-      else {
-        _setNoItemsInfo();
-      }
-      return this;
-    },
-    /** Clears the table body. */
-    clear: function() {
-      _setNoItemsInfo();
-      return this;
-    }
-  };
-}());
