6787c3230dbdb7f1a0abc3249e7b34ac554459a7
daq@gm2-01 <daq@gm2-01>
2019-11-27 12:06:19 -0600

    display Sten timing info

--- httpServer/public/javascripts/timing.js
+++ httpServer/public/javascripts/timing.js
@@ -1,54 +1,70 @@
 
-let pulser = ["1", "2", "3", "4"];
 let timingTab = document.getElementById("timingInfoTable");
-let attr = ["enable_2step", "charge_start", "step1_end",
-  "step2_start", "charge_end", "discharge_start", "discharge_end"];
-let rfAttr = ["width", "delay1", "delay2", "delay3", "delay4"];
-let state = ["active", "proposed"];
+
+var pulser, attr, rfAttr, state;
+if (window.controller === "BU") {
+  pulser = ["1", "2", "3", "4"];
+  attr = ["enable_2step", "charge_start", "step1_end",
+    "step2_start", "charge_end", "discharge_start", "discharge_end"];
+  rfAttr = ["width", "delay1", "delay2", "delay3", "delay4"];
+  state = ["active", "proposed"];
+}
+else {
+  attr = ["os", "fs", "fs2ss", "ss", "ss2dis", "dis"];
+}
 
 function showTimingInfo() {
   $.get(baseUrl + "/timing")
     .done((data)=>{
-      let pulserInfo = data.pulser;
-      for (var i = 0, len = pulser.length; i < len; i++) {
-        for (var k = 0, lenk = attr.length; k < lenk; k++) {
-          let row = timingTab.rows[k + 2];
-          for (var j = 0, lenj = state.length; j < lenj; j++) {
-            let colN = [1 + 2 * i + j];
-            let content = pulserInfo[pulser[i]][state[j]][attr[k]];
-            if (j === 0) { //active
-              if (!([2, 3].includes(k))) {
-                row.cells[colN].innerHTML = content;
-              }
-              else{
-                let is2Step = (pulserInfo[pulser[i]][state[j]][attr[0]]);
-                if (is2Step) 
+      if (window.controller === "BU") {
+        let pulserInfo = data.pulser;
+        for (var i = 0, len = pulser.length; i < len; i++) {
+          for (var k = 0, lenk = attr.length; k < lenk; k++) {
+            let row = timingTab.rows[k + 2];
+            for (var j = 0, lenj = state.length; j < lenj; j++) {
+              let colN = [1 + 2 * i + j];
+              let content = pulserInfo[pulser[i]][state[j]][attr[k]];
+              if (j === 0) { //active
+                if (!([2, 3].includes(k))) {
                   row.cells[colN].innerHTML = content;
-                else 
-                  row.cells[colN].innerHTML = "N/A";
+                }
+                else{
+                  let is2Step = (pulserInfo[pulser[i]][state[j]][attr[0]]);
+                  if (is2Step) 
+                    row.cells[colN].innerHTML = content;
+                  else 
+                    row.cells[colN].innerHTML = "N/A";
+                }
+              } else {
+                row.cells[colN].firstChild.defaultValue = (content);
               }
-            } else {
-              row.cells[colN].firstChild.defaultValue = (content);
+            }
+          }
+        }
+
+        let rfInfo = data.rf;
+        for (var i = 0, len = pulser.length; i < len; i++) {
+          for (var k = 0, lenk = rfAttr.length; k < lenk; k++) {
+            let row = timingTab.rows[k + 9];
+            for (var j = 0, lenj = state.length; j < lenj; j++) {
+              let colN = [1 + 2 * i + j];
+              let content = rfInfo[pulser[i]][rfAttr[k]];
+              if (j === 0)
+                row.cells[colN].innerHTML = content;
+              else
+                row.cells[colN].firstChild.defaultValue = content;
             }
           }
         }
       }
-
-      let rfInfo = data.rf;
-      for (var i = 0, len = pulser.length; i < len; i++) {
-        for (var k = 0, lenk = rfAttr.length; k < lenk; k++) {
-          let row = timingTab.rows[k + 9];
-          for (var j = 0, lenj = state.length; j < lenj; j++) {
-            let colN = [1 + 2 * i + j];
-            let content = rfInfo[pulser[i]][rfAttr[k]];
-            if (j === 0)
-              row.cells[colN].innerHTML = content;
-            else
-              row.cells[colN].firstChild.defaultValue = content;
-          }
+      else if (window.controller === "Sten") {
+        for (var iAttr = 0, lenAttr = attr.length; iAttr < lenAttr; iAttr++) {
+          let row = timingTab.rows[iAttr + 2];
+          let content = data[attr[iAttr]];
+          row.cells[1].innerHTML = content;
+          row.cells[2].firstChild.defaultValue = content;
         }
       }
-
     })
     .fail(()=>{
       alert("Could not get timing settings ...");
