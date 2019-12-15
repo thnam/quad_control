d02beec939ec88740f3275cb1b92f0c314803fbc
daq@gm2-01 <daq@gm2-01>
2019-12-14 23:52:18 -0600

    remove post method in timing route

--- monitor/routes/timing.js
+++ monitor/routes/timing.js
@@ -4,10 +4,15 @@ const httpLog = require(global.appRoot + '/loggers/httpLogger.js');
 const timingLog = require(global.appRoot + '/loggers/timingLogger.js');
 const exec = require('util').promisify(require('child_process').exec);
 const BUEnv = require('config').BUEnv;
+const controller = require('config').controller;
 
 router
   .get('/', async(req, res, next) => {
-    var cmd = global.appRoot + "/../hwInterface/bu/readPulserTiming";
+    var cmd;
+    if (controller === "BU")
+      cmd = global.appRoot + "/../hwInterface/bu/readPulserTiming";
+    else if (controller === "Sten")
+      cmd = global.appRoot + "/../hwInterface/lj/ljPulserTiming.py";
 
     exec(cmd, {env: BUEnv}).then((data)=>{
       httpLog.info("Read pulser timing successfully");
@@ -19,5 +24,4 @@ router
       res.status(500).send(err.stderr);
     });
   });
-
 module.exports = router;
